2da08ad8f8e1cdc23cfc79423bbf3c07d4d86e70
Niansong Zhang <niansong.zhang@uwaterloo.ca>
2021-06-20 12:06:19 +0800

    update

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 for ((CORE_NUM=1; CORE_NUM<=20; CORE_NUM++))
 do
     export MKL_NUM_THREADS=$CORE_NUM
-    taskset -c 1-$CORE_NUM python mbv1.py
+    taskset -c 0-$((CORE_NUM - 1)) python mbv1.py
 done
+
